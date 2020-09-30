46929e9985606dbb24ccfc3798e561eadf96202c
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-09-30 05:47:53 +0100

    in the root query, I now have access to the list of all users, using GraphQLList(UserType)

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -665,6 +665,12 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args){
                 return posts
             }
+        },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parent, args){
+                return users
+            }
         }
     }
 });
