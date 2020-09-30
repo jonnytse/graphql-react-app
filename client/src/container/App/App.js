ccffee440afaf0c29d76d175efc446e4fa0da6d9
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-09-30 14:37:38 +0100

    completed apollo set up in frontend

--- client/src/container/App/App.js
+++ client/src/container/App/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import styles from './App.module.scss';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+//components to import and nest in root component aka App.js
 import Posts from '../../components/Posts';
+
+//apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql';
+})
 
 class App extends Component {
   render(){
     return (
-      <div className={styles.App}>
-        <h1>H1 Header within 'App' component</h1>
-        <Posts />
-      </div>
+      <ApolloProvider client={client}>
+        <div className={styles.App}>
+          <h1>H1 Header within 'App' component</h1>
+          <Posts />
+        </div>
+      </ApolloProvider>
     );
   }
 }
