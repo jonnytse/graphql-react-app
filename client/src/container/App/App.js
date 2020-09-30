adf540b40bc44bcd1e498e910576c8d436a171b3
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-09-30 12:43:07 +0100

    tweaking H1 header wording in app.js

--- client/src/container/App/App.js
+++ client/src/container/App/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render(){
     return (
       <div className={styles.App}>
-        <h1>The H1 Header within App.js</h1>
+        <h1>H1 Header within App.js</h1>
       </div>
     );
   }
