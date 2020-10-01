811be428a9a6a56c132c0d6fb56d0d48ee3b4568
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-10-01 10:36:07 +0100

    adjusted the index.js in client side to revert back to original code, avoding the Provider and Playground imports, as suggested by blog.logrocket.com article by Ibrahima Ndaw

--- client/src/index.js
+++ client/src/index.js
@@ -5,24 +5,24 @@ import { Playground, store } from 'graphql-playground-react';
 import './index.css';
 import App from './container/App/App';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-const rootElement = document.getElementById('root')
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Playground endpoint='https://fakerql.nplan.io/' />
-      {/* <Playground endpoint='https://fakerql.com/graphql' /> */}
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
-  rootElement
-)
+  document.getElementById('root')
+);
+
+// const rootElement = document.getElementById('root')
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <Playground endpoint='https://fakerql.nplan.io/' />
+//       {/* <Playground endpoint='https://fakerql.com/graphql' /> */}
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   rootElement
+// )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
